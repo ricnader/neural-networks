73241a7b7cc1d3c4576f9d30863223fac826135d
ric.nader@gmail.com <ric.nader@gmail.com>
2023-09-15 17:52:05 -0300

    Example of normalization usage with keras layers

---
+++ examples/LayersWithNormalizedData.py
@@ -0,0 +1,82 @@
+import math
+from keras.activations import sigmoid
+from keras.losses import MeanSquaredError, BinaryCrossentropy
+from keras import Sequential
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import lab_coffee_utils as util
+from keras.layers import Dense, Input
+tf.autograph.set_verbosity(0)
+# from lab_utils_common import dlc
+# from lab_neurons_utils import plt_prob_1d, sigmoidnp, plt_linear, plt_logistic
+# import logging
+# logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+
+X, Y = util.load_coffee_data()
+
+#print(X)
+
+
+print(X.shape, Y.shape)
+
+
+#util.plt_roast(X, Y)
+
+print("")
+print("")
+
+print(f"Temperature Max, Min pre normalization: {np.max(X[:,0]):0.2f}, {np.min(X[:,0]):0.2f}")
+print(f"Duration    Max, Min pre normalization: {np.max(X[:,1]):0.2f}, {np.min(X[:,1]):0.2f}")
+norm_l = tf.keras.layers.Normalization(axis=-1)
+norm_l.adapt(X)  # learns mean, variance
+Xn = norm_l(X)
+print(f"Temperature Max, Min post normalization: {np.max(Xn[:,0]):0.2f}, {np.min(Xn[:,0]):0.2f}")
+print(f"Duration    Max, Min post normalization: {np.max(Xn[:,1]):0.2f}, {np.min(Xn[:,1]):0.2f}")
+
+
+Xt = np.tile(Xn,(1000,1))
+Yt= np.tile(Y,(1000,1))   
+print(Xt.shape, Yt.shape)   
+
+
+tf.random.set_seed(1234)  # applied to achieve consistent results
+model = Sequential(
+    [
+        tf.keras.Input(shape=(2,)),
+        Dense(3, activation='sigmoid', name = 'layer1'),
+        Dense(1, activation='sigmoid', name = 'layer2')
+     ]
+)
+print("")
+print("")
+
+model.summary()
+
+
+
+L1_num_params = 2 * 3 + 3   # W1 parameters  + b1 parameters
+L2_num_params = 3 * 1 + 1   # W2 parameters  + b2 parameters
+print("L1 params = ", L1_num_params, ", L2 params = ", L2_num_params  )
+
+
+W1, b1 = model.get_layer("layer1").get_weights()
+W2, b2 = model.get_layer("layer2").get_weights()
+print(f"W1{W1.shape}:\n", W1, f"\nb1{b1.shape}:", b1)
+print(f"W2{W2.shape}:\n", W2, f"\nb2{b2.shape}:", b2)
+
+
+print("")
+print("")
+
+
+model.compile(
+    loss = tf.keras.losses.BinaryCrossentropy(),
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+)
+
+model.fit(
+    Xt,Yt,            
+    epochs=10,
+)
